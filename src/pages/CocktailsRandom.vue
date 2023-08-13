64d67a2a3cd2a49f62a77bac5965fe3c282dddaa
alheym <alheym.mw@gmail.com>
2023-08-13 11:05:10 +0300

    feat: creation of main pages

---
+++ src/pages/CocktailsRandom.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>CoktailRandom</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main.sass'
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AppLayout from '../components/applayout.vue'
+</script>
+
+<template>
+  <AppLayout imgUrl="/src/assets/img/bg-1.jpg">
+    <div class="wrapper">
+      <div class="info">
+        <div class="title">Choose your drink</div>
+        <div class="line"></div>
+      </div>
+
+    </div>
+  </AppLayout>
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main.sass'
+
+.wrapper
+  display: flex
+  justify-content: center
+  align-items: center
+
+.info
+  padding: 5rem 0
+  text-align: center 
+</style>
