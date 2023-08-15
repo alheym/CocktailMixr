9e715a1130c58e03a1e01fa8d195eb7b70ce4f52
alheym <alheym.mw@gmail.com>
2023-08-15 10:43:16 +0300

    feat: getting cocktails from the server and displaying the list

---
+++ src/components/CocktailThumb.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+const props = defineProps({
+  cocktail: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="root">
+    <RouterLink :to="`/cocktails/${cocktail.idDrink}`">
+      <div :style="`background-image: url(${cocktail.strDrinkThumb})`" class="pic" />
+    </RouterLink>
+    <div class="name">{{ cocktail.strDrink }}</div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main'
+
+.root
+  width: calc((100% - 4.75rem)/3)
+  padding-right: 0.0625rem
+  padding-bottom: 1.25rem
+
+  &:nth-child(3n + 3)
+    padding-right: 0
+
+.pic 
+  width: 7.5rem
+  height: 7.5rem
+  margin: 0 auto
+  background-repeat: no-repeat
+  background-position: 50% 50%
+  background-size: 100%
+
+.name 
+  padding-top: 0.9375rem
+  letter-spacing: 0.1
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,47 @@
 <script setup>
-import AppLayout from '../components/applayout.vue'
+import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import AppLayout from '../components/AppLayout.vue'
+import CocktailThum from '../components/CocktailThumb.vue'
+import { useRootStore } from '@/stores/root'
+
+const rootStore = useRootStore()
+rootStore.getIngredients()
+
+const { ingredients, cocktails } = storeToRefs(rootStore)
+const ingredient = ref(null)
+
+function getCocktails() {
+  rootStore.getCocktails(ingredient.value)
+}
 </script>
 
 <template>
   <AppLayout imgUrl="/src/assets/img/bg-1.jpg">
     <div class="wrapper">
-      <div class="info">
+      <div v-if="!ingredient || !cocktails" class="info">
         <div class="title">Choose your drink</div>
         <div class="line"></div>
+        <div class="select-wrapper">
+          <el-select v-model="ingredient" placeholder="Choose main ingredient" size="large" class="select"
+            @change="getCocktails">
+            <el-option v-for="item in ingredients" :key="item.strIngredient1" :label="item.strIngredient1"
+              :value="item.strIngredient1" />
+          </el-select>
+        </div>
+        <div class="text">
+          Try our delicious cocktail recipes for every occasion. Find delicious cocktail recipes by ingredient through our
+          cocktail generator.
+        </div>
+        <img src="/src/assets/img/cocktails.png" alt="cocktails" class="img" />
       </div>
-
+      <div v-else class="info">
+        <div class="title">COCKTAILS WITH {{ ingredient }}</div>
+        <div class="line"></div>
+        <div class="cocktails">
+          <CocktailThum v-for="cocktail in cocktails" :key="cocktail.idDrink" :cocktail="cocktail" />
+        </div>
+      </div>
     </div>
   </AppLayout>
 </template>
@@ -25,4 +57,34 @@ import AppLayout from '../components/applayout.vue'
 .info
   padding: 5rem 0
   text-align: center 
+
+.select-wrapper 
+  padding-top: 3.125rem
+
+.select 
+  width: 13.75rem
+
+.text
+  max-width: 32.25rem
+  margin: 0 auto
+  padding-top: 3.125rem
+  line-height: 2.25rem
+  letter-spacing: 0.1em
+  color: $textMuted
+
+.img
+  margin-top: 3.75rem
+
+.cocktails
+  display: flex
+  justify-content: space-between
+  align-items: center
+  flex-wrap: wrap
+  max-height: 33rem
+  overflow-y: auto
+  margin-top: 3.75rem
+  
+  &::-webkit-scrollbar
+    display: none
+
 </style>
